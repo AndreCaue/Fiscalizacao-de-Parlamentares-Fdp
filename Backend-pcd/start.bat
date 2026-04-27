0bb6a0e055b3fab46d4302beb67bca52ca902f65
André C <andrecaue@outlook.com>
2026-04-27 20:47:04 -0300

    removing old archives

--- Backend-pcd/start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Iniciando o ambiente virtual...
-call .\venv\Scripts\activate
-echo Ambiente virtual ativado!
-echo Iniciando o servidor Uvicorn...
-uvicorn app.main:app --reload
-pause
--- Frontend-pcd/components/fiscalizacao/index.tsx
+++
@@ -1,5 +0,0 @@
-export { FiscalizacaoSection } from "./FiscalizacaoSection";
-export { RiscoScore } from "./RiscoScore";
-export { AlertasLista } from "./AlertList";
-export { PatrimonioChart } from "./PatrimonioChart";
-export { CEAPDashboard } from "./CEAPDashboard";
